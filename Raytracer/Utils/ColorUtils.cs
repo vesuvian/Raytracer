a038bbfae1604ab0d8c7f9118b3ae5b90263c710
Chris Cameron <chris.cameron@icdsystems.com>
2021-06-19 12:11:50 -0400

    perf: Optimized HSL to RGB conversion

--- Raytracer/Utils/ColorUtils.cs
+++ Raytracer/Utils/ColorUtils.cs
@@ -1,123 +1,116 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Numerics;
 
 namespace Raytracer.Utils
 {
-	public class ColorUtils
+	public static class ColorUtils
 	{
-		private const float Epsilon = 0.00001f;
+		private const float EPSILON = 0.00001f;
 
 		public static Color Lerp(Color a, Color b, float f)
 		{
-			var hslA = RgBtoHsl(a);
-			var hslB = RgBtoHsl(b);
-			Vector3 hslLerped = Vector3.Lerp(hslA, hslB, f);
-			float alphaLerped = MathUtils.Lerp(a.A, b.A, f);
+			Vector4 hslA = RgbToHsl(a);
+			Vector4 hslB = RgbToHsl(b);
+			Vector4 hslLerped = Vector4.Lerp(hslA, hslB, f);
 
-			return HsLtoRgb(hslLerped.X, hslLerped.Y, hslLerped.Z, alphaLerped);
+			return HslToRgb(hslLerped);
 		}
 
         /// <summary>
-        /// Converts RGB to HSL. Alpha is ignored.
-        /// Output is: { H: [0, 360], S: [0, 1], L: [0, 1] }.
+        /// Converts RGB to HSL.
         /// </summary>
         /// <param name="color">The color to convert.</param>
-        public static Vector3 RgBtoHsl(Color color)
+        public static Vector4 RgbToHsl(Color color)
         {
-            double h = 0D;
-            double s = 0D;
-            double l;
-            // normalize red, green, blue values
-            double r = color.R / 255D;
-            double g = color.G / 255D;
-            double b = color.B / 255D;
-            double max = System.Math.Max(r, System.Math.Max(g, b));
-            double min = System.Math.Min(r, System.Math.Min(g, b));
+	        // normalize red, green, blue values
+            float r = color.R / 255.0f;
+            float g = color.G / 255.0f;
+            float b = color.B / 255.0f;
+
+            float max = System.Math.Max(r, System.Math.Max(g, b));
+            float min = System.Math.Min(r, System.Math.Min(g, b));
+
             // hue
-            if (System.Math.Abs(max - min) < Epsilon)
-                h = 0D; // undefined
-            else if ((System.Math.Abs(max - r) < Epsilon)
-                    && (g >= b))
-                h = (60D * (g - b)) / (max - min);
-            else if ((System.Math.Abs(max - r) < Epsilon)
-                    && (g < b))
-                h = ((60D * (g - b)) / (max - min)) + 360D;
-            else if (System.Math.Abs(max - g) < Epsilon)
-                h = ((60D * (b - r)) / (max - min)) + 120D;
-            else if (System.Math.Abs(max - b) < Epsilon)
-                h = ((60D * (r - g)) / (max - min)) + 240D;
+            float h = 0;
+            if (System.Math.Abs(max - min) < EPSILON)
+                h = 0; // undefined
+            else if (System.Math.Abs(max - r) < EPSILON && g >= b)
+                h = 60 * (g - b) / (max - min);
+            else if (System.Math.Abs(max - r) < EPSILON && g < b)
+                h = 60 * (g - b) / (max - min) + 360;
+            else if (System.Math.Abs(max - g) < EPSILON)
+                h = 60 * (b - r) / (max - min) + 120;
+            else if (System.Math.Abs(max - b) < EPSILON)
+                h = 60 * (r - g) / (max - min) + 240;
+
             // luminance
-            l = (max + min) / 2D;
+            float l = (max + min) / 2;
+
             // saturation
-            if ((System.Math.Abs(l) < Epsilon)
-                    || (System.Math.Abs(max - min) < Epsilon))
-                s = 0D;
-            else if ((0D < l)
-                    && (l <= .5D))
+            float s = 0;
+            if (System.Math.Abs(l) < EPSILON || System.Math.Abs(max - min) < EPSILON)
+                s = 0;
+            else if (0 < l && l <= 0.5f)
                 s = (max - min) / (max + min);
-            else if (l > .5D)
-                s = (max - min) / (2D - (max + min)); //(max-min > 0)?
-            return new Vector3
-            (
-                MathF.Max(0f, MathF.Min(360f, float.Parse($"{h:0.##}"))),
-                MathF.Max(0f, MathF.Min(1f, float.Parse($"{s:0.##}"))),
-                MathF.Max(0f, MathF.Min(1f, float.Parse($"{l:0.##}")))
-            );
+            else if (l > 0.5f)
+                s = (max - min) / (2 - (max + min));
+
+            return new Vector4(MathUtils.Clamp(h, 0, 360),
+                               MathUtils.Clamp(s, 0, 1),
+                               MathUtils.Clamp(l, 0, 1),
+                               color.A);
         }
         /// <summary>
-        /// Converts HSL to RGB, with a specified output Alpha.
-        /// Arguments are limited to the defined range:
-        /// does not raise exceptions.
+        /// Converts HSL to RGB.
         /// </summary>
-        /// <param name="h">Hue, must be in [0, 360].</param>
-        /// <param name="s">Saturation, must be in [0, 1].</param>
-        /// <param name="l">Luminance, must be in [0, 1].</param>
-        /// <param name="a">Output Alpha, must be in [0, 1].</param>
-        public static Color HsLtoRgb(double h, double s, double l, double a = 1)
+        /// <param name="hsla"></param>
+        public static Color HslToRgb(Vector4 hsla)
         {
-            h = System.Math.Max(0D, System.Math.Min(360D, h));
-            s = System.Math.Max(0D, System.Math.Min(1D, s));
-            l = System.Math.Max(0D, System.Math.Min(1D, l));
-            a = System.Math.Max(0D, System.Math.Min(1D, a));
+	        float h = MathUtils.Clamp(hsla.X, 0, 360);
+            float s = MathUtils.Clamp(hsla.Y, 0, 1);
+            float l = MathUtils.Clamp(hsla.Z, 0, 1);
+            float a = MathUtils.Clamp(hsla.W, 0, 1);
+
             // achromatic argb (gray scale)
-            if (System.Math.Abs(s) < Epsilon)
+            if (System.Math.Abs(s) < EPSILON)
             {
-                return Color.FromArgb(
-                        System.Math.Max(0, System.Math.Min(255, Convert.ToInt32(double.Parse($"{a * 255D:0.00}")))),
-                        System.Math.Max(0, System.Math.Min(255, Convert.ToInt32(double.Parse($"{l * 255D:0.00}")))),
-                        System.Math.Max(0, System.Math.Min(255, Convert.ToInt32(double.Parse($"{l * 255D:0.00}")))),
-                        System.Math.Max(0, System.Math.Min(255, Convert.ToInt32(double.Parse($"{l * 255D:0.00}")))));
+                return Color.FromArgb((int)MathUtils.Clamp(a * 255, 0, 255),
+                                      (int)MathUtils.Clamp(l * 255, 0, 255),
+                                      (int)MathUtils.Clamp(l * 255, 0, 255),
+                                      (int)MathUtils.Clamp(l * 255, 0, 255));
             }
-            double q = l < .5D
-                    ? l * (1D + s)
-                    : (l + s) - (l * s);
-            double p = (2D * l) - q;
-            double hk = h / 360D;
-            double[] T = new double[3];
-            T[0] = hk + (1D / 3D); // Tr
-            T[1] = hk; // Tb
-            T[2] = hk - (1D / 3D); // Tg
+
+            float q = l < 0.5f ? l * (1 + s) : l + s - l * s;
+            float p = 2 * l - q;
+            float hk = h / 360;
+
+            float[] rgb =
+            {
+	            hk + 1 / 3.0f,
+	            hk,
+	            hk - 1 / 3.0f
+            };
+
             for (int i = 0; i < 3; i++)
             {
-                if (T[i] < 0D)
-                    T[i] += 1D;
-                if (T[i] > 1D)
-                    T[i] -= 1D;
-                if ((T[i] * 6D) < 1D)
-                    T[i] = p + ((q - p) * 6D * T[i]);
-                else if ((T[i] * 2D) < 1)
-                    T[i] = q;
-                else if ((T[i] * 3D) < 2)
-                    T[i] = p + ((q - p) * ((2D / 3D) - T[i]) * 6D);
+                if (rgb[i] < 0)
+                    rgb[i] += 1;
+                if (rgb[i] > 1)
+                    rgb[i] -= 1;
+                if (rgb[i] * 6 < 1)
+                    rgb[i] = p + (q - p) * 6 * rgb[i];
+                else if (rgb[i] * 2 < 1)
+                    rgb[i] = q;
+                else if (rgb[i] * 3 < 2)
+                    rgb[i] = p + (q - p) * (2 / 3.0f - rgb[i]) * 6;
                 else
-                    T[i] = p;
+                    rgb[i] = p;
             }
-            return Color.FromArgb(
-                    System.Math.Max(0, System.Math.Min(255, Convert.ToInt32(double.Parse($"{a * 255D:0.00}")))),
-                    System.Math.Max(0, System.Math.Min(255, Convert.ToInt32(double.Parse($"{T[0] * 255D:0.00}")))),
-                    System.Math.Max(0, System.Math.Min(255, Convert.ToInt32(double.Parse($"{T[1] * 255D:0.00}")))),
-                    System.Math.Max(0, System.Math.Min(255, Convert.ToInt32(double.Parse($"{T[2] * 255D:0.00}")))));
+
+            return Color.FromArgb((int)MathUtils.Clamp(a * 255, 0, 255),
+                                  (int)MathUtils.Clamp(rgb[0] * 255, 0, 255),
+                                  (int)MathUtils.Clamp(rgb[1] * 255, 0, 255),
+                                  (int)MathUtils.Clamp(rgb[2] * 255, 0, 255));
         }
     }
 }
