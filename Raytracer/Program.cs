fae818eafb54cd770809f13eab58651dded56a92
Chris Cameron <chris.cameron@icdsystems.com>
2021-06-19 10:58:07 -0400

    refactor: Decoupled camera features from buffer

--- Raytracer/Program.cs
+++ Raytracer/Program.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Numerics;
-using Raytracer.Extensions;
 using Raytracer.Math;
 using Raytracer.SceneObjects;
 using Raytracer.SceneObjects.Geometry;
@@ -18,10 +16,7 @@ namespace Raytracer
 	{
 		private const int WIDTH = 1920;
 		private const int HEIGHT = 1080;
-		private const float NEAR_PLANE = 0.01f;
-		private const float FAR_PLANE = 1000.0f;
-		private const float FOV = 100;
-		private const float RENDER_SCALE = 0.5f;
+		private const float RENDER_SCALE = 2.0f;
 
 		private const string PATH = @"C:\\Temp\\raytrace.bmp";
 
@@ -31,9 +26,12 @@ namespace Raytracer
 			{
 				Camera = new Camera
 				{
-					Position = new Vector3(0, 0, -10),
+					Position = new Vector3(0, 2, -10),
 					Rotation = Quaternion.CreateFromYawPitchRoll(MathUtils.DEG2RAD * 180, 0, 0),
-					//Projection = Matrix4x4.CreatePerspective(WIDTH, HEIGHT, NEAR_PLANE, FAR_PLANE)
+					NearPlane = 0.01f,
+					FarPlane = 20.0f,
+					Fov = 100,
+					Aspect = WIDTH / (float)HEIGHT
 				},
 				Lights = new List<Light>
 				{
@@ -49,13 +47,9 @@ namespace Raytracer
 						Position = new Vector3(3, 1, 7.5f),
 						Scale = new Vector3(2, 1, 1),
 						Rotation = Quaternion.CreateFromYawPitchRoll(MathUtils.DEG2RAD * 45, MathUtils.DEG2RAD * 15, MathUtils.DEG2RAD * 30),
-						Radius = 2
+						Radius = 5
 					},
-					new Plane
-					{
-						Position = new Vector3(0, 0, 0),
-						Rotation = Quaternion.CreateFromYawPitchRoll(0, MathUtils.DEG2RAD * 45, 0)
-					}
+					new Plane()
 				}
 			};
 
@@ -78,24 +72,16 @@ namespace Raytracer
 			Process.Start("cmd.exe", $"/c {PATH}");
 		}
 
-		public static void Render(Scene scene, Bitmap buffer)
+		private static void Render(Scene scene, Bitmap buffer)
 		{
-			float aspect = (float)buffer.Width / buffer.Height;
-			float scale = (float)System.Math.Tan(MathUtils.DEG2RAD * FOV * 0.5f);
-
 			for (int y = 0; y < buffer.Height; y++)
 			{
 				for (int x = 0; x < buffer.Width; x++)
 				{
-					float rayX = (2 * (x + 0.5f) / buffer.Width - 1) * aspect * scale;
-					float rayY = (1 - 2 * (y + 0.5f) / buffer.Height) * scale;
-					Vector3 direction = scene.Camera.LocalToWorld.MultiplyDirection(new Vector3(rayX, rayY, -1));
+					float xViewport = (x + 0.5f) / buffer.Width;
+					float yViewport = (y + 0.5f) / buffer.Height;
 
-					Ray ray = new Ray
-					{
-						Origin = scene.Camera.Position,
-						Direction = Vector3.Normalize(direction)
-					};
+					Ray ray = scene.Camera.CreateRay(xViewport, yViewport);
 
 					Color pixel = CastRay(scene, ray);
 					buffer.SetPixel(x, y, pixel);
@@ -103,7 +89,7 @@ namespace Raytracer
 			}
 		}
 
-		public static Color CastRay(Scene scene, Ray ray)
+		private static Color CastRay(Scene scene, Ray ray)
 		{
 			IGeometry closest = null;
 			Intersection? closestIntersection = null;
@@ -126,7 +112,8 @@ namespace Raytracer
 				return Color.Black;
 
 			float planarDistance = Plane.Distance(scene.Camera.Position, scene.Camera.Forward, closestIntersection.Value.Position, out _);
-			float t = MathUtils.Clamp(planarDistance, NEAR_PLANE, FAR_PLANE) / (FAR_PLANE - NEAR_PLANE);
+			float t = MathUtils.Clamp(planarDistance, scene.Camera.NearPlane, scene.Camera.FarPlane) /
+			          (scene.Camera.FarPlane - scene.Camera.NearPlane);
 			return ColorUtils.Lerp(Color.White, Color.Black, t);
 		}
 	}
--- Raytracer/SceneObjects/Camera.cs
+++ Raytracer/SceneObjects/Camera.cs
@@ -1,6 +1,40 @@
-﻿namespace Raytracer.SceneObjects
+﻿using System;
+using System.Numerics;
+using Raytracer.Extensions;
+using Raytracer.Math;
+using Raytracer.Utils;
+
+namespace Raytracer.SceneObjects
 {
 	public sealed class Camera : AbstractSceneObject
 	{
+		public float NearPlane { get; set; } = 0.001f;
+
+		public float FarPlane { get; set; } = 1000.0f;
+
+		public float Fov { get; set; } = 100.0f;
+
+		public float Aspect { get; set; } = 2.0f;
+
+		/// <summary>
+		/// Creates a camera ray for the given viewport co-ordinates in the range 0 - 1 (bottom left to top right).
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <returns></returns>
+		public Ray CreateRay(float x, float y)
+		{
+			float scale = (float)System.Math.Tan(MathUtils.DEG2RAD * Fov * 0.5f);
+
+			float rayX = (2 * x - 1) * Aspect * scale;
+			float rayY = (1 - 2 * y) * scale;
+			Vector3 direction = LocalToWorld.MultiplyDirection(new Vector3(rayX, rayY, -1));
+
+			return new Ray
+			{
+				Origin = Position,
+				Direction = Vector3.Normalize(direction)
+			};
+		}
 	}
 }
