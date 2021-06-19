f2cd528f888df55dd055962a8ad9fa90e1a33d6e
Chris Cameron <chris.cameron@icdsystems.com>
2021-06-19 19:12:40 -0400

    feat: Initial attempt at implementing depth of field

---
+++ Raytracer/Extensions/RandomExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Raytracer.Utils;
+
+namespace Raytracer.Extensions
+{
+	public static class RandomExtensions
+	{
+		/// <summary>
+		/// Gets the next random value as a float.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <returns></returns>
+		public static float NextFloat([NotNull] this Random extends)
+		{
+			return (float)extends.NextDouble();
+		}
+
+		/// <summary>
+		/// Gets the next random value as a float.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <param name="maxValue"></param>
+		/// <returns></returns>
+		public static float NextFloat([NotNull] this Random extends, float maxValue)
+		{
+			return extends.NextFloat(0, maxValue);
+		}
+
+		/// <summary>
+		/// Gets the next random value as a float.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <param name="minValue"></param>
+		/// <param name="maxValue"></param>
+		/// <returns></returns>
+		public static float NextFloat([NotNull] this Random extends, float minValue, float maxValue)
+		{
+			float random = extends.NextFloat();
+			return MathUtils.Lerp(minValue, maxValue, random);
+		}
+	}
+}
--- Raytracer/SceneObjects/Camera.cs
+++ Raytracer/SceneObjects/Camera.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 using Raytracer.Extensions;
 using Raytracer.Math;
 using Raytracer.Utils;
@@ -14,6 +15,10 @@ namespace Raytracer.SceneObjects
 		public float Fov { get; set; } = 100.0f;
 
 		public float Aspect { get; set; } = 2.0f;
+
+		public float FocalLength { get; set; } = 10.0f;
+
+		public float ApertureSize { get; set; } = 0.5f;
 
 		public Matrix4x4 Projection
 		{
@@ -31,17 +36,31 @@ namespace Raytracer.SceneObjects
 		/// <returns></returns>
 		public Ray CreateRay(float x, float y)
 		{
+			// Calculate the local viewport ray
 			float scale = (float)System.Math.Tan(MathUtils.DEG2RAD * Fov * 0.5f);
-
 			float rayX = (2 * x - 1) * Aspect * scale;
 			float rayY = (1 - 2 * y) * scale;
-			Vector3 direction = LocalToWorld.MultiplyDirection(new Vector3(rayX, rayY, -1));
+			Vector3 direction = Vector3.Normalize(new Vector3(rayX, rayY, -1));
+
+			// Find the focal point
+			Vector3 focalpoint = new Ray {Direction = direction}.PositionAtDelta(FocalLength);
+
+			// Offset the start position by a random amount for depth of field
+			int seed = HashCode.Combine(x, y);
+			Random random = new Random(seed);
+			Vector3 apertureOffset =
+				new Vector3(random.NextFloat(-0.5f, 0.5f),
+				            random.NextFloat(-0.5f, 0.5f),
+				            random.NextFloat(-0.5f, 0.5f)) * ApertureSize;
+
+			// Direction is now the direction from the offset position to the focal point
+			direction = Vector3.Normalize(focalpoint - apertureOffset);
 
 			return new Ray
 			{
-				Origin = Position,
-				Direction = Vector3.Normalize(direction)
-			};
+				Origin = apertureOffset,
+				Direction = direction
+			}.Multiply(LocalToWorld);
 		}
 	}
 }
