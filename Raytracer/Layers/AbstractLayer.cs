80cff40b20a44d3bf4fea5c8069b4a4012719ba9
Chris Cameron <chris.cameron@icdsystems.com>
2021-06-19 19:58:27 -0400

    perf: Parallelizing layer rendering

--- Raytracer/Layers/AbstractLayer.cs
+++ Raytracer/Layers/AbstractLayer.cs
@@ -1,23 +1,52 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
 using Raytracer.Math;
 
 namespace Raytracer.Layers
 {
 	public abstract class AbstractLayer : ILayer
 	{
+		// 128 threads
+		private const int BUCKETS_X = 16;
+		private const int BUCKETS_Y = 16;
+
 		public void Render(Scene scene, Bitmap buffer)
 		{
-			for (int y = 0; y < buffer.Height; y++)
+			int width = buffer.Width;
+			int height = buffer.Height;
+
+			IEnumerable<Rectangle> buckets = GetBuckets(width, height, BUCKETS_X, BUCKETS_Y);
+
+			Parallel.ForEach(buckets, bucket =>
 			{
-				for (int x = 0; x < buffer.Width; x++)
+				for (int y = bucket.Top; y < bucket.Bottom; y++)
 				{
-					float xViewport = (x + 0.5f) / buffer.Width;
-					float yViewport = (y + 0.5f) / buffer.Height;
+					for (int x = bucket.Left; x < bucket.Right; x++)
+					{
+						float xViewport = (x + 0.5f) / width;
+						float yViewport = (y + 0.5f) / height;
 
-					Ray ray = scene.Camera.CreateRay(xViewport, yViewport);
+						Ray ray = scene.Camera.CreateRay(xViewport, yViewport);
+						Color pixel = CastRay(scene, ray);
+						lock (buffer)
+							buffer.SetPixel(x, y, pixel);
+					}
+				}
+			});
+		}
 
-					Color pixel = CastRay(scene, ray);
-					buffer.SetPixel(x, y, pixel);
+		private static IEnumerable<Rectangle> GetBuckets(int width, int height, int bucketsX, int bucketsY)
+		{
+			int bucketWidth = width / bucketsX;
+			int bucketHeight = height / bucketsY;
+
+			for (int x = 0; x < bucketsX; x++)
+			{
+				for (int y = 0; y < bucketsY; y++)
+				{
+					// TODO - Better handle rects that don't divide evenly?
+					yield return new Rectangle(x * bucketWidth, y * bucketHeight, bucketWidth, bucketHeight);
 				}
 			}
 		}
