e56d1866552b332c1960aea2210980aa030361c8
Chris Cameron <chris.cameron@icdsystems.com>
2021-06-20 19:27:40 -0400

    fix: Cube intersection position and normal fixes, still issues with depth

--- Raytracer/SceneObjects/Geometry/Cube.cs
+++ Raytracer/SceneObjects/Geometry/Cube.cs
@@ -1,34 +1,30 @@
 ﻿using System;
 using System.Numerics;
 using Raytracer.Math;
-using Raytracer.Utils;
 
 namespace Raytracer.SceneObjects.Geometry
 {
-    public sealed class Cube : AbstractSceneGeometry
-    {
-	    public override bool GetIntersection(Ray ray, out Intersection intersection)
-	    {
-		    intersection = default;
+	public sealed class Cube : AbstractSceneGeometry
+	{
+		public override bool GetIntersection(Ray ray, out Intersection intersection)
+		{
+			intersection = default;
 
 			// First transform ray to local space.
-		    ray = ray.Multiply(WorldToLocal);
+			ray = ray.Multiply(WorldToLocal);
 
-		    float[] xt = CheckAxis(ray.Origin.X, ray.Direction.X);
-		    float[] yt = CheckAxis(ray.Origin.Y, ray.Direction.Y);
-		    float[] zt = CheckAxis(ray.Origin.Z, ray.Direction.Z);
+			float[] xt = CheckAxis(ray.Origin.X, ray.Direction.X);
+			float[] yt = CheckAxis(ray.Origin.Y, ray.Direction.Y);
+			float[] zt = CheckAxis(ray.Origin.Z, ray.Direction.Z);
 
-		    float tMin = MathF.Max(MathF.Max(xt[0], yt[0]), zt[0]);
-		    float tMax = MathF.Min(MathF.Min(xt[1], yt[1]), zt[1]);
+			float tMin = MathF.Max(MathF.Max(xt[0], yt[0]), zt[0]);
+			float tMax = MathF.Min(MathF.Min(xt[1], yt[1]), zt[1]);
 
-		    if (tMin > tMax)
-			    return false;
+			if (tMin > tMax)
+				return false;
 
-			// TODO - Calculate normal
-			Vector3 normal = Vector3.Negate(ray.Direction); //* MathUtils.SmoothStep(tMin, tMax, 1.0f);
-
-			// TODO - Calculate position
-			Vector3 pos = new Vector3();
+			Vector3 pos = ray.PositionAtDelta(tMin);
+			Vector3 normal = GetNormal(pos);
 
 			intersection = new Intersection
 			{
@@ -39,36 +35,58 @@ namespace Raytracer.SceneObjects.Geometry
 
 			intersection.Multiply(LocalToWorld);
 			return true;
-	    }
+		}
 
-	    private float[] CheckAxis(float origin, float direction)
-	    {
+		private static Vector3 GetNormal(Vector3 pos)
+		{
+			Vector3 cubeMin = Vector3.One * -1;
+			Vector3 cubeMax = Vector3.One;
+			Vector3 pointToMin = pos - cubeMin;
+			Vector3 pointToMax = pos - cubeMax;
 
-		    float[] t = new float[2];
+			if (MathF.Abs(pointToMin.X) < 0.0001f)
+				return new Vector3(-1, 0, 0);
+			if (MathF.Abs(pointToMax.X) < 0.0001f)
+				return new Vector3(1, 0, 0);
+			if (MathF.Abs(pointToMin.Y) < 0.0001f)
+				return new Vector3(0, -1, 0);
+			if (MathF.Abs(pointToMax.Y) < 0.0001f)
+				return new Vector3(0, 1, 0);
+			if (MathF.Abs(pointToMin.Z) < 0.0001f)
+				return new Vector3(0, 0, -1);
+			if (MathF.Abs(pointToMax.Z) < 0.0001f)
+				return new Vector3(0, 0, 1);
 
-		    float tMinNumerator = (-1 - origin);
-		    float tMaxNumerator = (1 - origin);
+			return default;
+		}
 
-		    //Infinities might pop here due to division by zero
-		    if (MathF.Abs(direction) >= 0.000001f)
-		    {
-			    t[0] = tMinNumerator / direction;
-			    t[1] = tMaxNumerator / direction;
-		    }
-		    else
-		    {
-			    t[0] = tMinNumerator * 1e10f;
-			    t[1] = tMaxNumerator * 1e10f;
-		    }
+		private static float[] CheckAxis(float origin, float direction)
+		{
+			float[] t = new float[2];
 
-		    if (t[0] > t[1])
-		    {
-			    float temp = t[0];
-			    t[0] = t[1];
-			    t[1] = temp;
-		    }
+			float tMinNumerator = (-1 - origin);
+			float tMaxNumerator = (1 - origin);
 
-		    return t;
-	    }
+			//Infinities might pop here due to division by zero
+			if (MathF.Abs(direction) >= 0.000001f)
+			{
+				t[0] = tMinNumerator / direction;
+				t[1] = tMaxNumerator / direction;
+			}
+			else
+			{
+				t[0] = tMinNumerator * 1e10f;
+				t[1] = tMaxNumerator * 1e10f;
+			}
+
+			if (t[0] > t[1])
+			{
+				float temp = t[0];
+				t[0] = t[1];
+				t[1] = temp;
+			}
+
+			return t;
+		}
 	}
 }
