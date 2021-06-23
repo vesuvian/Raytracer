608137f53849e306d1d4150b3e13fd9305eb1a36
Chris Cameron <chris.cameron@icdsystems.com>
2021-06-22 20:50:33 -0400

    feat: Added ambient light

---
+++ Raytracer/SceneObjects/Lights/AmbientLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using System.Numerics;
+using Raytracer.Utils;
+
+namespace Raytracer.SceneObjects.Lights
+{
+	public sealed class AmbientLight : AbstractLight
+	{
+		public override Color Sample(Vector3 position, Vector3 normal)
+		{
+			return ColorUtils.Multiply(Color, Intensity);
+		}
+
+		public override bool CanSee(Scene scene, Vector3 position)
+		{
+			return true;
+		}
+	}
+}
