3f1a01495e04e1ab8b386a15d704d6a5ece0fb69
Chris Cameron <chris.cameron@icdsystems.com>
2021-06-27 10:53:30 -0400

    feat: Implemented disc and quad geometry

---
+++ Raytracer/SceneObjects/Geometry/Disc.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using Raytracer.Math;
+
+namespace Raytracer.SceneObjects.Geometry
+{
+	public sealed class Disc : AbstractSceneGeometry
+	{
+		public float Radius { get; set; } = 0.5f;
+
+		private static readonly Vector3 s_Normal = new Vector3(0, 1, 0);
+
+		public override IEnumerable<Intersection> GetIntersections(Ray ray)
+		{
+			// First transform the ray into local space
+			ray = ray.Multiply(WorldToLocal);
+
+			float t;
+			if (!Plane.HitPlane(ray, out t))
+				yield break;
+
+			Vector3 position = ray.PositionAtDelta(t);
+			if (position.Length() > Radius)
+				yield break;
+
+			// Plane normal is flipped if the ray comes from behind
+			Vector3 normal = Vector3.Dot(s_Normal, ray.Direction) < 0 ? s_Normal : s_Normal * -1;
+
+			Vector2 uv = (new Vector2(position.X / Radius, position.Z / Radius) + Vector2.One) / 2;
+
+			yield return new Intersection
+			{
+				Normal = normal,
+				Position = position,
+				RayOrigin = ray.Origin,
+				Uv = uv
+			}.Multiply(LocalToWorld);
+		}
+	}
+}
---
+++ Raytracer/SceneObjects/Geometry/Quad.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Raytracer.Math;
+
+namespace Raytracer.SceneObjects.Geometry
+{
+	public sealed class Quad : AbstractSceneGeometry
+	{
+		public float Edge { get; set; } = 1;
+
+		private static readonly Vector3 s_Normal = new Vector3(0, 1, 0);
+
+		public override IEnumerable<Intersection> GetIntersections(Ray ray)
+		{
+			// First transform the ray into local space
+			ray = ray.Multiply(WorldToLocal);
+
+			float t;
+			if (!Plane.HitPlane(ray, out t))
+				yield break;
+
+			Vector3 position = ray.PositionAtDelta(t);
+			if (MathF.Abs(position.X) > Edge / 2 ||
+			    MathF.Abs(position.Z) > Edge / 2)
+				yield break;
+
+			// Plane normal is flipped if the ray comes from behind
+			Vector3 normal = Vector3.Dot(s_Normal, ray.Direction) < 0 ? s_Normal : s_Normal * -1;
+
+			Vector2 uv = (new Vector2(position.X / Edge, position.Z) / Edge) + (Vector2.One / 2);
+
+			yield return new Intersection
+			{
+				Normal = normal,
+				Position = position,
+				RayOrigin = ray.Origin,
+				Uv = uv
+			}.Multiply(LocalToWorld);
+		}
+	}
+}
