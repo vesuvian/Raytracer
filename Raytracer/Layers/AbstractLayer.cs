d8349c5dc3abbe1ff87717c40f61ba414da91b25
Chris Cameron <chris.cameron@icdsystems.com>
2021-07-05 11:33:13 -0400

    feat: Rendering pixels in "random" order, results in better time estimates and looks cool!

--- Raytracer/Layers/AbstractLayer.cs
+++ Raytracer/Layers/AbstractLayer.cs
@@ -41,7 +41,11 @@ namespace Raytracer.Layers
 
 			int pixelsComplete = 0;
 
-			Parallel.ForEach(Enumerable.Range(0, width * height), px =>
+			IEnumerable<int> pixels =
+				Enumerable.Range(0, width * height)
+				          .Select(px => FeistelNet(px, width, height));
+
+			Parallel.ForEach(pixels, px =>
 			{
 				int x = px % width;
 				int y = px / width;
@@ -65,5 +69,27 @@ namespace Raytracer.Layers
 		}
 
 		protected abstract Color CastRay(Scene scene, Ray ray, int rayDepth);
+
+		/// <summary>
+		/// Gets a "random" pixel for each input, only visiting each pixel once.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		/// <returns></returns>
+		private static int FeistelNet(int index, int width, int height)
+		{
+			for (int i = 0; i < 8; i++)
+			{
+				int l = (index / width) | 0;
+				int r = index % width;
+				int nl = r;
+				int f = (r * 356357 + i * 1234567) % height;
+				r = (l + f) % height;
+				l = nl;
+				index = height * l + r;
+			}
+			return index;
+		}
 	}
 }
