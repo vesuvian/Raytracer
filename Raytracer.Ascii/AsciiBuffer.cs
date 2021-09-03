a6b472f236badba1536c4502ebc0683c8ae87215
Chris Cameron <chris.cameron@wedoresi.com>
2021-09-03 17:01:19 -0400

    feat: Added ascii renderer project

---
+++ Raytracer.Ascii/AsciiBuffer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Raytracer.Buffers;
+using Raytracer.Utils;
+
+namespace Raytracer.Ascii
+{
+	public sealed class AsciiBuffer : AbstractBuffer
+	{
+		private static readonly Dictionary<ConsoleColor, Color> s_ConsoleColors =
+			new Dictionary<ConsoleColor, Color>
+			{
+				{ ConsoleColor.Black, Color.Black },
+				{ ConsoleColor.DarkBlue, Color.DarkBlue },
+				{ ConsoleColor.DarkGreen, Color.DarkGreen },
+				{ ConsoleColor.DarkCyan, Color.DarkCyan },
+				{ ConsoleColor.DarkRed, Color.DarkRed },
+				{ ConsoleColor.DarkMagenta, Color.DarkMagenta },
+				{ ConsoleColor.DarkYellow, Color.DarkGoldenrod },
+				{ ConsoleColor.Gray, Color.Gray },
+				{ ConsoleColor.DarkGray, Color.DarkGray },
+				{ ConsoleColor.Blue, Color.Blue },
+				{ ConsoleColor.Green, Color.Green },
+				{ ConsoleColor.Cyan, Color.Cyan },
+				{ ConsoleColor.Red, Color.Red },
+				{ ConsoleColor.Magenta, Color.Magenta },
+				{ ConsoleColor.Yellow, Color.Yellow },
+				{ ConsoleColor.White, Color.White }
+			};
+
+		public override int Height { get; }
+
+		public override int Width { get; }
+
+		public AsciiBuffer(int width, int height)
+		{
+			Width = width;
+			Height = height;
+		}
+
+		public override void Dispose()
+		{
+		}
+
+		public override void SetPixel(int x, int y, Color color)
+		{
+			ConsoleColor closest = ConsoleColor.Black;
+			float delta = float.MaxValue;
+
+			foreach (var kvp in s_ConsoleColors)
+			{
+				float thisDelta = CompareHsl(kvp.Value, color);
+				if (thisDelta >= delta)
+					continue;
+
+				closest = kvp.Key;
+				delta = thisDelta;
+			}
+
+			lock (this)
+			{
+				Console.CursorVisible = false;
+				Console.SetCursorPosition(x, y);
+				Console.ForegroundColor = closest;
+				Console.Write("\u2588");
+			}
+		}
+
+		private float CompareHsl(Color a, Color b)
+		{
+			var aRgb = ColorUtils.ToVectorRgba(a);
+			var bRgb = ColorUtils.ToVectorRgba(b);
+
+			var aHsl = ColorUtils.RgbToHsl(aRgb);
+			var bHsl = ColorUtils.RgbToHsl(bRgb);
+
+			return (aHsl - bHsl).Length();
+		}
+
+		public override Color GetPixel(int x, int y)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
---
+++ Raytracer.Ascii/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using Raytracer.Layers;
+using Raytracer.Materials;
+using Raytracer.Materials.Textures;
+using Raytracer.Parsers;
+using Raytracer.SceneObjects;
+using Raytracer.SceneObjects.Cameras;
+using Raytracer.SceneObjects.Geometry;
+using Raytracer.SceneObjects.Lights;
+using Raytracer.Utils;
+
+namespace Raytracer.Ascii
+{
+	public static class Program
+	{
+		private static int Width { get { return Console.WindowWidth; } }
+		private static int Height { get { return Console.WindowHeight; } }
+		
+		public static void Main()
+		{
+			Console.CursorVisible = false;
+
+			BitmapTexture normal = BitmapTexture.FromPath("Resources\\TexturesCom_Wall_Stone2_3x3_1K_normal.tif");
+
+			Scene scene = new Scene
+			{
+				GlobalIlluminationSamples = 4,
+				Camera = new PerspectiveCamera
+				{
+					Position = new Vector3(5, 2, -20),
+					NearPlane = 0.01f,
+					FarPlane = 40.0f,
+					Fov = 40,
+					Samples = int.MaxValue,
+					FocalLength = 18,
+					ApertureSize = 0.2f,
+					Aspect = (Width / (float)Height) / 2
+				},
+				Lights = new List<ILight>
+				{
+					new PointLight
+					{
+						Position = new Vector3(10, 100, -5),
+						Color = new Vector4(5, 0, 0, 1),
+						Range = 200,
+						SoftShadowRadius = 2,
+						Falloff = eFalloff.Linear
+					},
+					new PointLight
+					{
+						Position = new Vector3(0, 10, 10),
+						Color = new Vector4(0, 5, 0, 1),
+						Range = 40,
+						SoftShadowRadius = 2,
+						Falloff = eFalloff.Linear
+					},
+					new PointLight
+					{
+						Position = new Vector3(0, 1, -20),
+						Color = new Vector4(5, 5, 5, 1),
+						Range = 80,
+						SoftShadowRadius = 2,
+						Falloff = eFalloff.Linear
+					}
+				},
+				Geometry = new List<ISceneGeometry>
+				{
+					new Sphere
+					{
+						Radius = 100000,
+						RayMask = eRayMask.Visible,
+						Material = new EmissiveMaterial
+						{
+							Emission = BitmapTexture.FromPath("Resources\\skysphere.jpg")
+						}
+					},
+					new Cube
+					{
+						Position = new Vector3(13f, 5, 0),
+						Scale = new Vector3(2, 2, 2),
+						Rotation = Quaternion.CreateFromYawPitchRoll(MathUtils.DEG2RAD * 45, MathUtils.DEG2RAD * 45, MathUtils.DEG2RAD * 45),
+						Material = new LambertMaterial
+						{
+							Diffuse = new CheckerboardTexture { ColorA = new Vector4(0.9f), ColorB = new Vector4(0.1f) },
+							Normal = normal,
+						}
+					},
+					new Sphere
+					{
+						Position = new Vector3(3, 1, 7.5f),
+						Scale = new Vector3(2, 1, 1),
+						Rotation = Quaternion.CreateFromYawPitchRoll(MathUtils.DEG2RAD * 45, MathUtils.DEG2RAD * 15, MathUtils.DEG2RAD * 30),
+						Radius = 5,
+						Material = new LayeredMaterial
+						{
+							Blend = new SolidColorTexture { Color = new Vector4(0.5f) },
+							A = new LambertMaterial { Diffuse = new SolidColorTexture { Color = new Vector4(0.5f) } },
+							B = new ReflectiveMaterial { Roughness = new SolidColorTexture { Color = new Vector4(0.5f) } }
+						}
+					},
+					new Sphere
+					{
+						Position = new Vector3(-3, 10, 0),
+						Radius = 5,
+						Material = new LambertMaterial
+						{
+							Diffuse = new CheckerboardTexture { ColorA = new Vector4(0.9f), ColorB = new Vector4(0.1f) },
+							Normal = normal,
+							Scale = new Vector2(1 / 3.0f, 1)
+						}
+					},
+					new Sphere
+					{
+						Position = new Vector3(-4, 2, 0),
+						Radius = 2,
+						Material = new LayeredMaterial
+						{
+							Blend = new CheckerboardTexture(),
+							A = new LambertMaterial {
+								Diffuse = new CheckerboardTexture
+								{
+									ColorA = new Vector4(0.9f),
+									ColorB = new Vector4(0.1f)
+								},
+								Scale = new Vector2(1 / 3.0f, 1)
+							},
+							B = new ReflectiveMaterial(),
+							Scale = new Vector2(1 / 3.0f, 1)
+						}
+					},
+					new Sphere
+					{
+						Position = new Vector3(0, 2, 0),
+						Radius = 2,
+
+						Material = new LayeredMaterial
+						{
+							Blend = new SolidColorTexture { Color = new Vector4(0.5f) },
+							A = new LambertMaterial {
+								Diffuse = new CheckerboardTexture { ColorA = new Vector4(0.9f), ColorB = new Vector4(0.1f) },
+								Scale = new Vector2(1 / 3.0f, 1)
+							},
+							B = new ReflectiveMaterial()
+						}
+					},
+					new Sphere
+					{
+						Position = new Vector3(4, 2, 0),
+						Radius = 2,
+						Material = new ReflectiveMaterial()
+					},
+					new Sphere
+					{
+						Position = new Vector3(8, 2, 0),
+						Radius = 2,
+						Material = new ReflectiveMaterial
+						{
+							Roughness = new CheckerboardTexture
+							{
+								ColorA = new Vector4(0.5f)
+							},
+							Scale = new Vector2(1 / 3.0f, 1)
+						}
+					},
+					new Sphere
+					{
+						Position = new Vector3(12, 2, 0),
+						Radius = 2,
+						Material = new LambertMaterial
+						{
+							Diffuse = new SolidColorTexture { Color = new Vector4(0, 1, 0, 1) }
+						}
+					},
+					new Sphere
+					{
+						Position = new Vector3(16, 2, 0),
+						Radius = 2,
+						Material = new EmissiveMaterial
+						{
+							Emission = new SolidColorTexture { Color = new Vector4(0, 0, 1000, 1) }
+						}
+					},
+					new SceneObjects.Geometry.Plane
+					{
+						Material = new LambertMaterial
+						{
+							Diffuse = new CheckerboardTexture { ColorA = new Vector4(0.9f), ColorB = new Vector4(0.1f) },
+							Normal = normal,
+							Scale = new Vector2(5, 5)
+						}
+					},
+					new Model
+					{
+						Scale = Vector3.One * 0.2f,
+						Position = new Vector3(3, 2, -5),
+						Rotation = Quaternion.CreateFromYawPitchRoll(MathUtils.DEG2RAD * -45, MathUtils.DEG2RAD * -15, MathUtils.DEG2RAD * 30),
+						Mesh = new ObjMeshParser().Parse("Resources\\teapot.obj"),
+						Material = new LayeredMaterial
+						{
+							Blend = new SolidColorTexture { Color = new Vector4(0.5f) },
+							A = new LambertMaterial
+							{
+								Normal = normal,
+								Diffuse = new SolidColorTexture { Color = new Vector4(0.5f) }
+							},
+							B = new ReflectiveMaterial()
+						}
+					}
+				},
+				Layers = new List<ILayer>
+				{
+					new MaterialsLayer()
+				}
+			};
+
+			Parallel.ForEach(scene.Layers,
+			                 layer =>
+			                 {
+				                 Render(scene, layer);
+			                 });
+		}
+
+		private static void Render(Scene scene, ILayer layer)
+		{
+			using (AsciiBuffer buffer = new AsciiBuffer(Width, Height))
+			{
+				layer.Render(scene, buffer);
+			}
+		}
+	}
+}
