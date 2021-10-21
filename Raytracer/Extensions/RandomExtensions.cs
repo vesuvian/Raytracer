f2403f948ffba0dfc0c714a87e504fda8bf79ac1
Chris Cameron <chris.cameron@icdsystems.com>
2021-10-20 22:22:07 -0400

    feat: Added extension methods for generating random vectors

--- Raytracer/Extensions/RandomExtensions.cs
+++ Raytracer/Extensions/RandomExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
 using Raytracer.Utils;
 
 namespace Raytracer.Extensions
@@ -38,6 +39,41 @@ namespace Raytracer.Extensions
 		{
 			float random = extends.NextFloat();
 			return MathUtils.Lerp(minValue, maxValue, random);
+		}
+
+		/// <summary>
+		/// Gets the next random value as a Vector3.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <returns></returns>
+		public static Vector3 NextVector3(this Random extends)
+		{
+			return extends.NextVector3(Vector3.One);
+		}
+
+		/// <summary>
+		/// Gets the next random value as a Vector3.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <param name="maxValue"></param>
+		/// <returns></returns>
+		public static Vector3 NextVector3(this Random extends, Vector3 maxValue)
+		{
+			return extends.NextVector3(Vector3.Zero, maxValue);
+		}
+
+		/// <summary>
+		/// Gets the next random value as a Vector3.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <param name="minValue"></param>
+		/// <param name="maxValue"></param>
+		/// <returns></returns>
+		public static Vector3 NextVector3(this Random extends, Vector3 minValue, Vector3 maxValue)
+		{
+			return new Vector3(extends.NextFloat(minValue.X, maxValue.X),
+			                   extends.NextFloat(minValue.Y, maxValue.Y),
+			                   extends.NextFloat(minValue.Z, maxValue.Z));
 		}
 	}
 }
